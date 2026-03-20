aed2de48b4dce1431d99d0e4386f27b65e72d109
Float <floatingfloatskies@protonmail.com>
2026-03-20 14:36:06 -0400

    Change Docker installation to use Snap
    Replaced Docker installation method from APT to Snap.

--- post_install_fslab.sh
+++ post_install_fslab.sh
@@ -20,19 +20,6 @@ sudo snap install insomnia --classic
 sudo snap install datagrip --classic
 
 
-echo "Instalando Docker via repositorio Docker oficial"
-sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
+echo "Instalando Docker via repositorio snap oficial"
+sudo snap refresh
+sudo snap install docker
